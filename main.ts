9f86a5133aeff92db3c3c5d90fa3f990025ea076
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-03-29 18:30:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//11. Names: 
+//Store the names of a few of your friends in an array called names. Print each person’s name by 
+//accessing each element in the list, one at a time
+let names : string[]= ["Kabir", "Iqbal", "Ali", "Rehman", "Hamza"]
+//For loop 
+//For each 
+//For of loop 
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+///for each
+names.forEach(friends => {
+    console.log(friends);
+});
+//for of loop
+for (const friend1 of names) {
+    console.log(friend1);
+}
